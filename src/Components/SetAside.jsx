bdab7d9837b557a5427eed6f23df85cbbc4a75d0
Adrian Ross <adriantoddross@gmail.com>
2019-05-08 15:30:51 -0400

    add default state

--- src/Components/SetAside.jsx
+++ src/Components/SetAside.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 export default class SetAside extends React.Component() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            grossincome: 0,
+            netincome: 0,
+            taxwithheld: 0
+        }
+    }
+
     render() {
         return(
             <div>
-                
+
             </div>
         );
     }
