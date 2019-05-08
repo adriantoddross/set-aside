c9eeebbaf0652739961f6511706ee2ed7aafefa7
Adrian Ross <adriantoddross@gmail.com>
2019-05-08 15:49:46 -0400

    Remove imports

--- src/Components/SetAside.jsx
+++ src/Components/SetAside.jsx
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import Input from './Input';
-import Select from './Select';
-import Button from './Button';
-import GetPercentage from '../Utils/Percentage';
 
 export default class SetAside extends React.Component() {
     constructor(props) {
