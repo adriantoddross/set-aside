30c728f9c16c43285d8fb8d2187970370fd87400
Adrian Ross <adriantoddross@gmail.com>
2019-05-08 15:55:07 -0400

    Make component stateless

--- src/Components/SetAside.jsx
+++ src/Components/SetAside.jsx
@@ -1,24 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class SetAside extends React.Component() {
-    constructor(props) {
-        super(props);
-        this.state = {
-            grossIncome: 0,
-            netIncome: 0,
-            taxWithheld: 0
-        }
-    }
+export default function SetAside (props) {
 
-    render() {
-         
-        const gross = this.state.grossIncome;
-        const net = this.state.netIncome;
-        const taxes = this.state.taxWithheld;
-
-        return ( 
-            <div className='App'>
-            </div>
-        );
-    }
-}
+    return ( 
+        <div className='App'>
+        </div>
+    );
+};
