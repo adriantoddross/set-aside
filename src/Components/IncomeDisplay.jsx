59580dfc6ea9231740d551a0a1a2385472e781f0
Adrian Ross <adriantoddross@gmail.com>
2019-05-10 10:52:10 -0400

    remove dollar symbol

--- src/Components/IncomeDisplay.jsx
+++ src/Components/IncomeDisplay.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function incomeDisplay(props) {
      return (
          <div>
-             <p>${props.amount}</p>
+             <p>{props.amount}</p>
              <h2>{props.description}</h2>
          </div>
      );
