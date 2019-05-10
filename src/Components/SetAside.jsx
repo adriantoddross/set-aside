379a2a5714dcc56204e3fa50e8dde79840fffae2
Adrian Ross <adriantoddross@gmail.com>
2019-05-10 11:00:58 -0400

    convert to class

--- src/Components/SetAside.jsx
+++ src/Components/SetAside.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Form from './FormContainer';
 import IncomeDisplay from './IncomeDisplay';
 import formatToDollar from '../Utils/FormatToDollar.js';
 
-export default function SetAside (props) {
-    const taxWithheld = formatToDollar.format(1500);
-    const netIncome = formatToDollar.format(3000);
+export default class SetAside extends Component {
+    constructor(props) {
+        super(props);
 
-    return ( 
-        <div className='App'>
-            <Form/>
-            <IncomeDisplay
-                amount={taxWithheld}
-                description="Amount to withhold"
-            />
-            <IncomeDisplay
-                amount={netIncome}
-                description="Net income"
-            />
-        </div>
-    );
+        this.grabData = this.grabData.bind(this)
+    };
+
+    grabData(data) {
+        return data;
+    }
+
+    render() {
+        const taxWithheld = formatToDollar.format(1500);
+        const netIncome = formatToDollar.format(3000);
+        
+        return ( 
+            <div className='App'>
+                <Form handleData={this.grabData}/>
+                <IncomeDisplay
+                    amount={taxWithheld}
+                    description="Amount to withhold"
+                />
+                <IncomeDisplay
+                    amount={netIncome}
+                    description="Net income"
+                />
+            </div>
+        );
+    }
+    
 };
