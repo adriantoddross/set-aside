c434c855174e70aa95bc2ce9b63c18099fcb3d46
Adrian Ross <adriantoddross@gmail.com>
2019-05-10 12:02:33 -0400

    wire together callback with new helper method

--- src/Components/SetAside.jsx
+++ src/Components/SetAside.jsx
@@ -2,21 +2,37 @@ import React, {Component} from 'react';
 import Form from './FormContainer';
 import IncomeDisplay from './IncomeDisplay';
 import formatToDollar from '../Utils/FormatToDollar.js';
+import getPercentage from '../Utils/Percentage';
 
 export default class SetAside extends Component {
     constructor(props) {
         super(props);
 
-        this.getFormData = this.getFormData.bind(this)
+        this.state = {
+            grossIncome: 0,
+            tax: 0,
+            taxWithheld: 0,
+            netIncome: 0
+        }
+        this.getFormData = this.getFormData.bind(this);
+        this.calculateAmounts = this.calculateAmounts.bind(this);
     };
 
+    calculateAmounts(data) {
+        const withheld = getPercentage(data.grossIncome, data.tax);
+        const net = data.grossIncome - withheld;
+        this.setState({netIncome: net, taxWithheld: withheld});
+    }
+
     getFormData(data) {
-        console.log(data);
+        this.setState({...data});
+        this.calculateAmounts(data);
     }
 
     render() {
-        const taxWithheld = formatToDollar.format(1500);
-        const netIncome = formatToDollar.format(3000);
+        const taxWithheld = formatToDollar.format(this.state.taxWithheld);
+        const netIncome = formatToDollar.format(this.state.netIncome);
+        
         
         return ( 
             <div className='App'>
