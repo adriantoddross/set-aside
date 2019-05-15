e0de7368639a316061d1ffeca9589cff960e3d52
Adrian Ross <adriantoddross@gmail.com>
2019-05-15 16:30:21 -0400

    add classes

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -3,16 +3,17 @@ import React from 'react';
 export default function Select(props) {
     return (
         <div className="form-group">
-            <label htmlFor={props.name}> {props.title} </label>
-            <select
+            <label className="form-label" htmlFor={props.name}> {props.title} </label>
+            <select className="form-select"
               name={props.name}
               value={props.value}
               onChange={props.handleChange}
               >
-              <option value="" disabled>{props.placeholder}</option>
+              <option className="form-option" value="" disabled>{props.placeholder}</option>
               {props.options.map(option => {
                 return (
                   <option
+                    className="form-options"
                     key={option}
                     value={option}
                     label={option}>{option}
