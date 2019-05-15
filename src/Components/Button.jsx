8ee65e3fbfd95a4a6411d04613092cea588a694b
Adrian Ross <adriantoddross@gmail.com>
2019-05-15 15:05:20 -0400

    add parent div

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 export default function Button(props) {
     return (
-        <button 
-            style= {props.style} 
-            onClick= {props.action}
-            disabled= {!props.valid}
-        >    
-            {props.title} 
-        </button>
+        <div className="form-button">
+            <button 
+                style= {props.style} 
+                onClick= {props.action}
+                disabled= {!props.valid}
+            >    
+                {props.title} 
+            </button>
+        </div>
     );
 }
