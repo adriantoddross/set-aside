68bb55510a6fa1112587388585e67ec48051236d
Adrian Ross <adriantoddross@gmail.com>
2019-05-15 15:06:01 -0400

    change className

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Button(props) {
     return (
-        <div className="form-button">
+        <div className="form-submit">
             <button 
                 style= {props.style} 
                 onClick= {props.action}
