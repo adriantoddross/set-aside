4824e9c6bd94a114701d3c36418dc095d7335ad3
Adrian Ross <adriantoddross@gmail.com>
2019-05-16 13:51:10 -0400

    display headers before amount

--- src/Components/IncomeDisplay.jsx
+++ src/Components/IncomeDisplay.jsx
@@ -4,8 +4,8 @@ import './IncomeDisplay.css';
 export default function incomeDisplay(props) {
      return (
          <div className="income-display">
-             <p>{props.amount}</p>
              <h2>{props.description}</h2>
+             <p>{props.amount}</p>
          </div>
      );
 }
