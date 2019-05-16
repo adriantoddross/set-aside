b2eec8fa878d30d1878d874b9acda86571eb4e92
Adrian Ross <adriantoddross@gmail.com>
2019-05-16 17:08:38 -0400

    condense p tags

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,13 +9,7 @@ export default function Footer(props) {
           <img className='footer-img' src="https://res.cloudinary.com/adriantoddross/image/upload/v1527785141/adrian-ross.png" alt="Adrian Ross"/>
         </div>
         <p>
-          Designed and developed by <a className='attribution' href="https://www.adriantoddross.com" target="_blank" rel="noopener noreferrer">Adrian Ross</a>, <span className='attribution'>inspired by <a className='attribution' href="https://www.daveramsey.com/blog/file-taxes-for-freelance-work" target="_blank" rel="noopener noreferrer">Dave Ramsey</a></span>.
-        </p>
-        <p>
-          Hosted on Netlify. Code is licensed under MIT and available on Github. All images and graphics are licensed under CC BY-SA.
-        </p>
-        <p>
-          © 2019 Adrian Ross
+          Designed and developed by <a className='attribution' href="https://www.adriantoddross.com" target="_blank" rel="noopener noreferrer">Adrian Ross</a>, <span className='attribution'>inspired by <a className='attribution' href="https://www.daveramsey.com/blog/file-taxes-for-freelance-work" target="_blank" rel="noopener noreferrer">Dave Ramsey</a></span>. Hosted on Netlify. Code is licensed under MIT and available on Github. All images and graphics are licensed under CC BY-SA. © 2019 Adrian Ross
         </p>
       </footer>
     </div>
