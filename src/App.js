fbf31ec5cbe2d7eeee90fd62df6cd0fa2830ff0d
Adrian Ross <adriantoddross@gmail.com>
2019-05-17 13:59:43 -0400

    import Navbar and change main app class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Navbar from './Components/Navbar.jsx';
 import SetAsideApp from './Components/SetAside.jsx';
 import Footer from './Components/Footer.jsx';
+
 function App() {
   return (
-    <div className="App">
-      <SetAsideApp/>
-      <Footer/>
+    <div className="main-app">
+      <Navbar />
+      <SetAsideApp />
+      <Footer />
     </div>
   );
 }
