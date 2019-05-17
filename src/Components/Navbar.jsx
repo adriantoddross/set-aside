72abac998635cc9fc5b26c338bbd1523c6fafa1f
Adrian Ross <adriantoddross@gmail.com>
2019-05-17 13:57:01 -0400

    create navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Navbar() {
+  return(
+    <div className='navbar'>
+      <div>
+        <h1>Set Aside</h1>
+        <p>Easily determine your self-employed taxes.</p>
+      </div>
+      <ul>
+        <li><a className='attribution' href="https://github.com/adriantoddross/set-aside" target="_blank" rel="noopener noreferrer">Learn More</a></li>
+        <li><a className='attribution' href="https://github.com/adriantoddross/set-aside" target="_blank" rel="noopener noreferrer">GitHub Repo</a></li>
+      </ul>
+    </div>
+  )
+}
