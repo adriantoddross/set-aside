0efa1aebe5ba3979d35886aea519907c04d260a6
Adrian Ross <adriantoddross@gmail.com>
2019-05-17 14:01:44 -0400

    add unique clases to header & links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,11 +4,11 @@ import './Navbar.css';
 export default function Navbar() {
   return(
     <div className='navbar'>
-      <div>
+      <header className='header'>
         <h1>Set Aside</h1>
         <p>Easily determine your self-employed taxes.</p>
-      </div>
-      <ul>
+      </header>
+      <ul className='navlinks'>
         <li><a className='attribution' href="https://www.daveramsey.com/blog/file-taxes-for-freelance-work" target="_blank" rel="noopener noreferrer">Learn More</a></li>
         <li><a className='attribution' href="https://github.com/adriantoddross/set-aside" target="_blank" rel="noopener noreferrer">GitHub Repo</a></li>
       </ul>
